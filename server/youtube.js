362d75b89a219bc7c704a4955975ce00b68e76e7
SlackingPalico <joeldado08@gmail.com>
2015-09-29 16:52:25 -0700

    [feat] add server/youtube v2.0

--- server/youtube.js
+++ server/youtube.js
@@ -1,21 +1,57 @@
 var request = require('request');
+var key = require('./keys');
 
 var youtube = 'https://www.googleapis.com/youtube/v3';
 
-module.exports = function(itemToSearch, callback){
+module.exports = {
+  search : function(request, response) {
+    var itemToSearch = req.body.searchTerm;
+    // if (req.body.date === 'no present') {
+      
+    // }
+    
+    callYoutube(date, itemToSearch, function(status, data){
+      response.status(status).send(data);
+    })
+  }
+}
+
+//To grab videos on a specific month just add publishedBefore and publishedAfter in the query
+
+function callYoutube(item, callback){
   var search = {
     url : youtube + '/search',
-    qs : {part : 'snippet', type : 'video', q : 'just do it!', key : 'AIzaSyAGBNXHZthCIWxva4mtnG1TU_Yo0za5xKk'},
+    qs : {part : 'snippet', type : 'video', videoEmbeddable : true, q : item, key : key.youtube},
     method : 'GET'
   }
   
   request(search, function(error, response, body){
     if(error) {
       console.log(error);
-      callback(error);
+      callback(404, error);
     } else {
       body = JSON.parse(body);
-      callback("<div id='player'></div><script>var tag = document.createElement('script');tag.src = 'https://www.youtube.com/iframe_api';var firstScriptTag = document.getElementsByTagName('script')[0];firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);var player;function onYouTubeIframeAPIReady() {player = new YT.Player('player', {height: '390',width: '640',videoId: '" + body.items[0].id.videoId + "',events: {'onReady': onPlayerReady,'onStateChange': onPlayerStateChange}});}function onPlayerReady(event) {event.target.playVideo();}var done = false;function onPlayerStateChange(event) {if (event.data == YT.PlayerState.PLAYING && !done) {setTimeout(stopVideo, 6000);done = true;}}function stopVideo() {player.stopVideo();}</script>");
+      
+      //snippet.thumbnails.(key) | key = default|medium|high | access through key.url
+      var date = body.items[0].snippet.publishedAt.slice(0, 10); //Should be user defined
+      
+      var data = {};
+      data[date] = {
+        source : 'youtube',
+        children : []
+      }
+      
+      for (var i = 0; i < body.items.length && data[date].children.length <= 5; i++){
+        if (body.items[i].snippet.publishedAt.slice(0, 10) === date){
+          data[date].children.push({
+            title : body.items[i].snippet.title,
+            id : body.items[i].id.videoId,
+            thumbnail : body.items[i].snippet.thumbnails
+          })
+        }
+      }
+      
+      callback(200, data);
     }
   })
 }
