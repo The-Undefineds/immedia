edb7612c0d8fe6ab71c1177d64c98d1e7451937f
Jackson Sierra <jks718@gmail.com>
2015-09-30 11:44:32 -0700

    [refactor] (client) refactor ResultsView to use State for async ajax requests

--- client/src/results.jsx
+++ client/src/results.jsx
@@ -12,69 +12,47 @@ var ResultsView = React.createClass({
 
   getInitialState: function(){
     return {
-      WasSearchedFromTopBar: false,
+      wasSearchedFromTopBar: false,
       searchTerm: '',
-      rerender: false,
+      nyt: {},
+      youtube: {},
+      wikipedia: {},
+      twitter: {},
     };
   },
 
-  queryResults: {
-    nyt: {
-        // '2015-09-29':{
-        //   source: 'nyt',
-        //   children: [
-        //     {
-        //       title: 'Elon Musk',
-        //       img: 'https://lh5.googleusercontent.com/-89xTT1Ctbrk/AAAAAAAAAAI/AAAAAAAAAAA/5gt6hkVvJHY/s0-c-k-no-ns/photo.jpg'
-        //     }, {
-        //       title: 'Sergie Brin',
-        //       img: 'http://www.technologytell.com/gadgets/files/2014/05/Sergey-Brin-should-have-stayed-away-from-Google-Plus.jpg'
-        //     }
-        //   ]
-        // }
-      },
-    youtube: {},
-    twitter: {
-        // '2015-09-29': {
-        //   source: 'twitter',
-        //   children: [
-        //   {
-        //     title: 'Donald Trump',
-        //     img: 'http://uziiw38pmyg1ai60732c4011.wpengine.netdna-cdn.com/wp-content/dropzone/2015/08/RTX1GZCO.jpg'
-        //   }, {
-        //     title: 'The Donald',
-        //     img: 'http://www.liberationnews.org/wp-content/uploads/2015/07/donaldtrump61815.jpg'
-        //   }
-        //   ]
-        // }
-      },
-    wikipedia: {},
-  },
+  apis: [
+    'nyt',
+    // 'wikipedia',
+    // 'twitter',
+    'youtube'
+  ],
   
   componentDidMount: function(){
-
-    for(var id in this.queryResults){
+    for(var i = 0; i < this.apis.length; i++){
       //not the best way of reasoning about this, will refactor after MVP.
-      var searchTerm = WasSearchedFromTopBar ? this.state.searchTerm: this.props.searchTerm
+      var searchTerm = this.state.wasSearchedFromTopBar ? this.state.searchTerm: this.props.searchTerm;
       this.handleQuery({
         searchTerm: this.props.searchTerm,
-        url: 'http://127.0.0.1:3000/api/' + id
-      })
+        url: 'http://127.0.0.1:3000/api/' + this.apis[i],
+        api: this.apis[i]
+      });
     }
   },
 
   handleQuery: function(searchQuery){
-    $.post( searchQuery.url, searchQuery, function(data){
-      var source = data.source;
-      this.queryResults[source] = data;
-      this.setState({rerender: !this.state.rerender});
-    }, 'json')
+    $.post(searchQuery.url, searchQuery)
+     .done(function(response) {
+        var obj = {};
+        obj[searchQuery.api] = response;
+        this.setState(obj);
+     }.bind(this));
   },
 
   queryTerm: function(searchTerm){
     this.setState({
       searchTerm: searchTerm,
-      WasSearchedFromTopBar: true,
+      wasSearchedFromTopBar: true,
     });
   },
   // mouseOver: function(){
@@ -86,17 +64,20 @@ var ResultsView = React.createClass({
   // },
 
   render: function(){
+    console.log('rendering results');
     return (
       <div>
         <h1>Results</h1>
         <HomeButton />
         <TopBar queryTerm={this.queryTerm}/>
-        <TreeTimeLine data={this.queryResults}/>
-        <WikiView data={this.queryResults.wikipedia}/>
-        <ImageView data={this.queryResults.wikipedia} />
+        <TreeTimeLine data={
+          { nyt: this.state.nyt, twitter: this.state.twitter, youtube: this.state.youtube }
+        }/>
+        <WikiView data={this.state.wikipedia}/>
+        <ImageView data={this.state.wikipedia} />
         <Preview />
       </div>
-      )
+    );
   }
 });
 
