f165aaafccef15491dc633eda84fc30f65352b38
mtcrushmore <ryanthesmith@gmail.com>
2015-09-30 19:05:47 -0700

    [feature] mouseover in D3 canvas will render previewin results view

--- client/src/results_page/preview.jsx
+++ client/src/results_page/preview.jsx
@@ -2,10 +2,29 @@ var React = require('react');
 
 var Preview = React.createClass({
 
-  render: function(){
+  render: function() {
+
+    console.log('preview is rendered with item:', this.props.previewItem)
+    
+    var previewItem = this.props.previewItem;
+
+    // $.ajax({
+    //   type: 'GET',
+    //   url: 'http://' + previewItem.url,
+    //   dataType: 'jsonp',
+    //   success: function(data) {
+    //     console.log('success!', data);
+    //   },
+    //   failure: function(data) {
+    //     console.log('whateva');
+    //   }
+    // })
+
     return (
-      <div id='player'></div>
-      )
+      <div>
+        <h1>{ previewItem.title }</h1>
+      </div>  
+      );
   }
 });
 
--- client/src/results_page/treetimeline.jsx
+++ client/src/results_page/treetimeline.jsx
@@ -39,6 +39,83 @@ var d3data = {
   ]
 };
 
+var queryResults = {
+    nyt: {
+        '2015-09-29':{
+          source: 'nyt',
+          children: [
+            {
+              title: 'Elon Musk',
+              url: 'www.tesla.com',
+              img: 'https://lh5.googleusercontent.com/-89xTT1Ctbrk/AAAAAAAAAAI/AAAAAAAAAAA/5gt6hkVvJHY/s0-c-k-no-ns/photo.jpg'
+            }, {
+              title: 'Sergie Brin',
+              url: 'www.google.com',
+              img: 'http://www.technologytell.com/gadgets/files/2014/05/Sergey-Brin-should-have-stayed-away-from-Google-Plus.jpg'
+            }
+          ]
+        },
+        '2015-09-27':{
+          source: 'nyt',
+          children: [
+            {
+              title: 'Pope Francis',
+              url: 'www.thepope.com',
+              img: 'http://m.snopes.com/wp-content/uploads/2015/07/pope-francis.jpg'
+            }, {
+              title: 'Barack Obama',
+              url: 'www.whitehouse.gov',
+              img: 'https://pbs.twimg.com/profile_images/1645586305/photo.JPG'
+            }
+          ]
+        }
+      },
+    youtube: {
+        '2015-09-29': {
+          source: 'youtube',
+          children: [
+          {
+            title: 'Nickelback',
+            url: 'www.reddit.com',
+            img: 'http://www.blogcdn.com/www.joystiq.com/media/2008/11/nickelback.jpg'
+          }, {
+            title: 'Creed',
+            url: 'www.youtube.com',
+            img: 'http://www.ew.com/sites/default/files/i/daily/629//creed_l.jpg'
+          }
+        ]
+      }
+    },
+    twitter: {
+        '2015-09-29': {
+          source: 'twitter',
+          children: [
+          {
+            title: 'Donald Trump',
+            url: 'www.trump.com',
+            img: 'http://uziiw38pmyg1ai60732c4011.wpengine.netdna-cdn.com/wp-content/dropzone/2015/08/RTX1GZCO.jpg'
+          }, {
+            title: 'The Donald',
+            img: 'http://www.liberationnews.org/wp-content/uploads/2015/07/donaldtrump61815.jpg'
+          }
+          ]
+        },
+        '2015-09-27': {
+          source: 'twitter',
+          children: [
+          {
+            title: 'Kim Kardashian',
+            img: 'http://media2.popsugar-assets.com/files/2014/11/04/920/n/1922153/961af48ae3ec88c3_thumb_temp_cover_file23876211415134157.xxxlarge/i/Kim-Kardashian-Bleached-Brows.jpg'
+          }, {
+            title: 'Rihanna',
+            img: 'http://i.huffpost.com/gen/2717304/images/o-RIHANNA-DIOR-facebook.jpg'
+          }
+          ]
+        },
+      },
+    wikipedia: {},
+  }
+
 var TreeTimeLine = React.createClass({
 
   render: function() {
@@ -47,7 +124,18 @@ var TreeTimeLine = React.createClass({
     )
   },
 
+  mouseOver: function(item) {
+    this.props.mouseOver({
+        title: item.title,
+        date: item.date,
+        url: item.url,
+        img: item.img,
+      });
+  },
+
   renderCanvas: function() {
+    var component = this;
+
     var margin = {
       top: 40,
       right: 40,
@@ -152,7 +240,10 @@ var TreeTimeLine = React.createClass({
           toggle(d); 
           update(d); 
         })
-        .on("mouseover", function(d) {
+        .on("mouseenter", function(d) {
+          if (d.title) {
+            component.mouseOver(d);
+          }
           nodeEnter.append("svg:text")
             .text(function(d) { return d.title; })
             .attr({
@@ -163,7 +254,7 @@ var TreeTimeLine = React.createClass({
         })
         .on("mouseout", function(d) {
             nodeEnter.selectAll('text')
-              .style('fill-opacity', 0);
+              .remove()
         })
 
       var defs = svg.append('svg:defs');
