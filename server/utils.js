5fac7a462d9627e491b151c48b0eedf40c214895
Jackson Sierra <jks718@gmail.com>
2015-10-01 11:58:26 -0700

    [refactor] (server) add more date helper functions to utils.js

--- server/utils.js
+++ server/utils.js
@@ -4,11 +4,26 @@ module.exports = {
     delimiter = delimiter || '';
     var today = new Date();
     var desiredDate = new Date(today.setDate(today.getDate() + days));
-    var year = desiredDate.getFullYear().toString();
-    var month = (((desiredDate.getMonth()+1).toString()[1]) ? desiredDate.getMonth()+1 : "0" + (desiredDate.getMonth()+1)).toString();
-    var day = ((desiredDate.getDate().toString()[1]) ? desiredDate.getDate() : "0" + desiredDate.getDate()).toString();
+    var simpleDesiredDate = this.getSimpleDate(desiredDate);
 
-    return year + delimiter + month + delimiter + day;
+    return simpleDesiredDate.year + delimiter + simpleDesiredDate.month + delimiter + simpleDesiredDate.day;
+  },
+
+  'correctDateInFuture': function(date, delimiter) {
+    var correctedDate = new Date(date);
+    var today = new Date();
+    correctedDate = correctedDate > today ? today : correctedDate;
+    var simpleCorrectedDate = this.getSimpleDate(correctedDate);
+
+    return simpleCorrectedDate.year + delimiter + simpleCorrectedDate.month + delimiter + simpleCorrectedDate.day;
+  },
+
+  'getSimpleDate': function(date) {
+    return {
+      year: date.getFullYear().toString(),
+      month: (((date.getMonth()+1).toString()[1]) ? date.getMonth()+1 : "0" + (date.getMonth()+1)).toString(),
+      day: ((date.getDate().toString()[1]) ? date.getDate() : "0" + date.getDate()).toString()
+    };
   }
 
 };
