6bf3179a7ddb0009ceb1ad76a8d4e9fc0370735a
Jackson Sierra <jks718@gmail.com>
2015-10-01 20:50:58 -0700

    [refactor] (server) show tweets with highest social impact, add profile image

--- server/twitter.js
+++ server/twitter.js
@@ -1,5 +1,6 @@
 var OAuth = require('./OAuth');
 var request = require('request');
+var utils = require('./utils.js');
 
 var baseUrl = 'https://api.twitter.com/1.1/users/search.json';
 var newUrl = 'https://api.twitter.com/1.1/statuses/user_timeline.json';
@@ -17,7 +18,7 @@ var search = {
 module.exports = {
   getTweetsPerson : function(request, response){
     var queryString = request.body.searchTerm;
-    
+
     findUser(baseUrl, queryString, function(status, data){
       response.status(status).send(data);
     });
@@ -33,10 +34,13 @@ function findUser(baseUrl, queryString, callback){
       callback(404, error);
     } else {
       body = JSON.parse(body);
-      var id;
+      var id,
+          img;
       if (body instanceof Array) { 
         id = body[0].id;
-        grabTimeline(newUrl, id, callback);
+        img = body[0].profile_image_url;
+
+        grabTimeline(newUrl, {'id': id, 'img': img}, callback);
       }
       else {
         callback(200, body);
@@ -45,37 +49,46 @@ function findUser(baseUrl, queryString, callback){
   })
 }
 
-function grabTimeline(newUrl, id, callback){
+function grabTimeline(newUrl, params, callback){
   search.url = newUrl;
-  search.qs = {user_id : id, include_rts : 'false'};
-  search.headers.Authorization = OAuth(newUrl, 'user_id=' + id, 'include_rts=false');
+  search.qs = {user_id : params.id, include_rts : 'false'};
+  search.headers.Authorization = OAuth(newUrl, 'user_id=' + params.id, 'include_rts=false');
   request(search, function(error, response, body){
     if(error) {
       callback(404, error);
     } else {
-      body = JSON.parse(body);
-       if (body[0]) {
-         var arr = [];
-         var date = body[0].created_at;
-         var month = date.slice(4, 7);
-         var year = date.slice(26);
-         var day = date.slice(8,10);
-         date = year + '-' + month + '-' + day
-         
-         for (var i = 0; i < 3; i++){
-          arr.push(body[i]["id_str"]);
-         }
-         embedTweet(url, arr, callback, date);
-       }
-       else callback(200, body);
+      body = Array.prototype.slice.call(JSON.parse(body));
+      if (body[0]) {
+        var tweetIdsToSend = [];
+        var tweetsBySocialCount = {};
+        var date = utils.getSimpleDate(body[0].created_at);
+        date = date.year + '-' + date.month + '-' + date.day;
+
+        for (var i = 0; i < body.length; i++){
+          var socialCount = body[i]['retweet_count'] + body[i]['favorite_count'];
+          tweetsBySocialCount[socialCount] = body[i]['id_str'];
+        }
+
+        var topTweetCounts = Object.keys(tweetsBySocialCount).sort(function(a, b) {
+          return b - a;
+        }).slice(0, 3);
+
+        for(var j = 0; j < topTweetCounts.length; j++) {
+          tweetIdsToSend.push(tweetsBySocialCount[topTweetCounts[j]]);
+        }
+
+        embedTweet(url, tweetIdsToSend, callback, {'date': date, 'img': params.img});
+      } else {
+        callback(200, body);
+      }
     }
   })
 }
 
-function embedTweet(url, arr, callback, date){
+function embedTweet(url, tweetIds, callback, params){
   var obj = {};
   var manualPromise = 0;
-  obj[date] = {
+  obj[params.date] = {
     source : 'twitter',
     children : []
   }
@@ -83,16 +96,17 @@ function embedTweet(url, arr, callback, date){
   search.url = url;
   search.headers.Authorization = null;
   
-  for (var i = 0; i < arr.length; i++){
-    search.qs = {id : arr[i]};
+  for (var i = 0; i < tweetIds.length; i++){
+    search.qs = {id : tweetIds[i]};
     request(search, function(error, response, body){
       if(error) {
         callback(404, error);
       } else {
         body = JSON.parse(body);
-        obj[date].children.push({
+        obj[params.date].children.push({
           url : body.url,
-          tweet : body.html
+          tweet : body.html,
+          img: params.img
         })
         manualPromise++;
         if (manualPromise === 3) callback(200, obj);
