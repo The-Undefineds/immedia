5f17ab2f0c7bb44adb26c10b3b1ba7fa2ab130d1
Jackson Sierra <jks718@gmail.com>
2015-10-06 14:18:00 -0700

    [style] (client) change font from a default Apple one to a Google CDN

--- client/src/results_page/emptypreview.jsx
+++ client/src/results_page/emptypreview.jsx
@@ -3,7 +3,7 @@ var StyleSheet = require('react-style');
 
 var styles = StyleSheet.create({
   directions: {
-    fontFamily: 'Avenir',
+    fontFamily: 'Nunito',
     fontSize: '24px',
     color: '#00BFFF',
   }
