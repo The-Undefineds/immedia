3fa80f279e4a336d1cbfdfd06155c784be9edd35
Jackson Sierra <jks718@gmail.com>
2015-10-05 20:13:16 -0700

    [refactor] (client) remove Embed.ly commented code from NYTPreview

--- client/src/results_page/nytpreview.jsx
+++ client/src/results_page/nytpreview.jsx
@@ -1,23 +1,14 @@
 var React = require('react');
+var StyleSheet = require('react-style');
 
-// var frameCache = {};
-
-var styles = {
+var styles = StyleSheet.create({
   anchor: {
     textDecoration: 'none',
     color: 'inherit',
   }
-};
+});
 
 var NytPreview = React.createClass({
-
-  // getInitialState: function () {
-  //   return {
-  //     rerender: false,
-  //   }
-  // },
-  
-  // resultsData: {},
 
   render: function() {
     var ratio = 1;
@@ -43,39 +34,8 @@ var NytPreview = React.createClass({
         </div>
       </a>
     );
+  },
 
-    // component = this;
-    
-    // var previewItem = this.props.previewItem;
-    // var preview = false;
-
-    // if (!frameCache[previewItem.url]) {
-    //   var queryUrl = 'http://api.embed.ly/1/oembed?key=7fd3eb5aa6f34be7be07cb9015f01e23&url=' + previewItem.url;
-    //   $.get(queryUrl, function(data) {
-    //       frameCache[previewItem.url] = data;
-    //       this.resultsData = data;
-    //       this.resultsData.thumbnail_height = 0.45*this.resultsData.thumbnail_height;
-    //       this.resultsData.thumbnail_width = 0.45*this.resultsData.thumbnail_width;
-    //       component.setState({ rerender: !component.state.rerender })
-    //   })
-    // } else {
-    //   this.resultsData = frameCache[previewItem.url];
-    //   preview = true;
-    // }
-
-
-
-    // return (
-    //   <div>
-    //     <h1>{ this.resultsData.title }</h1>
-    //     <h3>{ this.resultsData.author_name }</h3>
-    //     <img src={ this.resultsData.thumbnail_url } height={ this.resultsData.thumbnail_height } width= { this.resultsData.thumbnail_width }></img>
-    //     <p>{ this.resultsData.description }</p>
-    //   </div>
-    //   );
-  }
-
-
-})
+});
 
 module.exports = NytPreview;
