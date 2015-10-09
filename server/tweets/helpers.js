5c9ab9b465cd873f21641831ed72e370d31dbae8
SlackingPalico <joeldado08@gmail.com>
2015-10-08 17:14:20 -0700

    [feat] added parse logic for each tweets

--- server/tweets/helpers.js
+++ server/tweets/helpers.js
@@ -13,7 +13,6 @@ var months = {
   Dec: 12
 };
 
-
 var extractUrl = function(tweet){
   for (var i = tweet.length-1; i >= 0; i--) {
     if (tweet[i] === 'h' && tweet.slice(i, i+5) === 'http:') {
@@ -29,10 +28,69 @@ var convertDateToInteger = function(date){
   return year * 480 + month * 40 + day * 1;
 };
 
-var parse ....
+var parseText = function(tweet){
+  
+  //Removed ending text that consists of links
+  var index = tweet.indexOf('http');
+  tweet = tweet.slice(0, index);
+  
+  //Removes all symbols
+  var fixedTweet = tweet.replace(/([.,'"*+?^=!:${}()|\[\]\/\\])/g, " "),
+      newTweet = [],
+      topic = [],
+      obj = {},
+      i;
+
+  fixedTweet = fixedTweet.split(' ');
+  
+  for (i = 0; i < fixedTweet.length; i++){
+    if (fixedTweet[i] === '') continue;
+    var changed = false;
+    var indexes = [];
+    
+    for (var z = 1; z < fixedTweet[i].length; z++){
+      if (fixedTweet[i][z] === fixedTweet[i][z].toUpperCase()){
+        indexes.push(fixedTweet[i].indexOf(fixedTweet[i][z]));
+        changed = true;
+      }
+    }
+    
+    if (!changed) newTweet.push(fixedTweet[i].toLowerCase());
+    else {
+      newTweet.push(fixedTweet[i].slice(0, indexes[0]));
+      for (z = 0; z < indexes.length; z++){
+        if (z + 1 === indexes.length){
+          newTweet.push(fixedTweet[i].slice(indexes[z]).toLowerCase());
+          continue;
+        }
+        newTweet.push(fixedTweet[i].slice(indexes[z], indexes[z + 1]).toLowerCase());
+      }
+    }
+  }
+
+  for (i = 0; i < newTweet.length; i++){
+    if (assets.blackListedWords[newTweet[i]]) continue;
+    
+    if (!(obj[newTweet[i]])){
+      topic.push(newTweet[i]);
+      obj[newTweet[i]] = true;
+    }
+    
+    if (i + 1 !== newTweet.length) {
+      if (assets.blackListedWords[newTweet[i + 1]]) continue;
+      if (!(obj[newTweet[i] + ' ' + newTweet[i + 1]])){
+        topic.push(newTweet[i] + ' ' + newTweet[i + 1]);
+        obj[newTweet[i] + ' ' + newTweet[i + 1]] = true;
+      }
+    }
+  }
+  
+  return topic;
+};
 
 
 module.exports = {
   extractUrl: extractUrl,
-  convertDateToInteger: convertDateToInteger
+  convertDateToInteger: convertDateToInteger,
+  parseText: parseText
 };
