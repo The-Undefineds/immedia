712b08f00f37f718ae3a13868a71eb373e8ffcdb
Jackson Sierra <jks718@gmail.com>
2015-10-21 22:55:00 -0700

    [docs] (server) add comments to Google Images middleware

--- server/googleImages.js
+++ server/googleImages.js
@@ -1,3 +1,11 @@
+/*
+    file: googleImages.js
+    - - - - - - - - - - - - - - 
+    Middleware that sends a request to Google's
+    Custom Search API for only image results. This
+    handling is used only in the event that a Wikipedia
+    article does not include a photo in the Infobox.
+ */
 var keys = require('./keys.js');
 var request = require('request');
 var url = require('url');
@@ -10,10 +18,16 @@ module.exports = {
     var googleCustomApiUrl = 'https://www.googleapis.com/customsearch/v1?';
     var queryString = googleCustomApiUrl + 'q=' + searchTerm + '&cx=' + keys.googleImagesEngineID + '&imgType=photo&key=' + keys.googleImages;
 
-    request(queryString, function(error, response) {
-      if(error) console.error(error);
+    request(queryString, function(error, response, body) {
+      if(error) res.status(404).send(error);
 
-      var body = JSON.parse(response.body);
+      if(body) {
+        body = JSON.parse(body);
+      } else {
+        res.status(204).send('No images for that query');
+      }
+
+      // Gets the top image result for the given query
       if(body['items']) {
         for(var key in body['items']) {
           var pageMap = body['items'][key]['pagemap'];
