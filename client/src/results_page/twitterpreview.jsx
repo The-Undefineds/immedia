ee0a7a48e4fcf1ce0006469bd9185fea062b371a
Jackson Sierra <jks718@gmail.com>
2015-10-21 23:11:27 -0700

    [docs] (client) split TwitterPreview styles into separate file and add comments

--- client/src/results_page/twitterpreview.jsx
+++ client/src/results_page/twitterpreview.jsx
@@ -1,35 +1,24 @@
+/*
+    file: twitterpreview.jsx
+    - - - - - - - - - - - - - 
+    Separate component for displaying
+    Tweets selected for preview.
+
+    Content for preview is passed through as a prop
+    (previewItem) since it is received from the
+    TreeTimeLine.
+
+    Furthermore, this component requires custom embed
+    widget code provided by Twitter which asynchronously
+    embeds a previewed Tweet.
+ */
+
+// Required node modules
 var React = require('react');
-var StyleSheet = require('react-style');
 
-//This is code supplied by the Twitter API documentation
-//Exposes several methods, including .createTweet, which can be used to embed tweets asynchronously
-//based on the tweet ID alone.
-window.twttr = (function(d, s, id) {
+// React StyleSheet styling
+var styles = require('../styles/results_page/twitterpreview.jsx');
 
-  var js, fjs = d.getElementsByTagName(s)[0],
-    t = window.twttr || {};
-  if (d.getElementById(id)) return t;
-  js = d.createElement(s);
-  js.id = id;
-  js.src = "https://platform.twitter.com/widgets.js";
-  fjs.parentNode.insertBefore(js, fjs);
- 
-  t._e = [];
-  t.ready = function(f) {
-    t._e.push(f);
-  };
- 
-  return t;
-}(document, "script", "twitter-wjs"));
-
-var styles = StyleSheet.create({
-  preview: {
-    top: '40px',
-    paddingRight: '5px',
-    position: 'absolute',
-    overflow: 'scroll',
-  },
-});
 
 var TwitterPreview = React.createClass({
 
@@ -38,6 +27,10 @@ var TwitterPreview = React.createClass({
       width: this.props.window.width,
       height: this.props.window.height,
     }
+  },
+
+  componentWillMount: function() {
+    this.instantiateTwitterWidget();
   },
 
   componentWillReceiveProps: function(nextProps) {
@@ -73,14 +66,41 @@ var TwitterPreview = React.createClass({
     styles.preview.height = this.state.height - 100 + 'px';
   },
 
-  embedTweet: function(tweetId) {
+  /*
+      This is code supplied by the Twitter API documentation
+      It exposes several methods, including .createTweet, which can be used to embed tweets asynchronously
+      based on the tweet ID alone.
+   */
+  instantiateTwitterWidget: function() {
+    window.twttr = (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0],
+        t = window.twttr || {};
+      if (d.getElementById(id)) return t;
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "https://platform.twitter.com/widgets.js";
+      fjs.parentNode.insertBefore(js, fjs);
+     
+      t._e = [];
+      t.ready = function(f) {
+        t._e.push(f);
+      };
+     
+      return t;
+    }(document, "script", "twitter-wjs"));
+    return;
+  },
 
-    //A new div with id "tweet" is created, and the embedded tweet is appended to it.
+  //A new div with id "tweet" is created, and the embedded tweet is appended to it.
+  embedTweet: function(tweetId) {
     $('<div id="tweet"></div>').hide().prependTo('#previewTwitter').fadeIn(800);
+    
+    twttr.ready(function(twttr) {
       twttr.widgets.createTweet(
         tweetId,
         document.getElementById('tweet')
       );
+    });
   },
 });
 
---
+++ client/src/styles/results_page/twitterpreview.jsx
@@ -0,0 +1,12 @@
+var StyleSheet = require('react-style');
+
+var TwitterPreviewStyles = StyleSheet.create({
+  preview: {
+    top: '40px',
+    paddingRight: '5px',
+    position: 'absolute',
+    overflow: 'scroll',
+  },
+});
+
+module.exports = TwitterPreviewStyles;
