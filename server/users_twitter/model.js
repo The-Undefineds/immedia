8d81a951676c0595e04bd7070a0d9cc8de2c956a
Jackson Sierra <jks718@gmail.com>
2015-10-21 22:44:48 -0700

    [refactor] (server) remove extraneous files not used in deployment

--- server/users_twitter/model.js
+++
@@ -1,9 +0,0 @@
-var mongoose  = require('mongoose');
-
-var UserSchema = new mongoose.Schema({
-  search_term: {type: String, unique: true},
-  user_id: String,
-  img: String
-});
-
-module.exports = mongoose.model('users', UserSchema);
